1888e9041cc946e6668d0f92fd4eddb0fe2b632f
kushagra rathore <kushagrarathore002@gmail.com>
2020-12-22 19:25:45 +0530

    changed scroll

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,196 +1,134 @@
-import React, { Component } from 'react';
-import Styles from  './Item.module.css';
-import axios from 'axios';
-import Search from './Search';
-import List from './List';
-const maxresult=7;
+import React, { Component,createRef} from "react";
+import Styles from "./Item.module.css";
+import axios from "axios";
+import Search from "./Search";
+import List from "./List";
+
+
+const maxresult = 7;
 class Item extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            things:"Loading...",
-            videoList: [],
-            toggle:"Show",
-            video:{},
-            description:``,
-            text:"covid 19 news",
-            title:""
-
-        }}
-        youtube=async()=>
-        {
-            
-        await axios.get(`https://www.googleapis.com/youtube/v3/search`,{
-            params:{
-                part:"snippet",
-                key:"AIzaSyCranKkgzC8Bui0dlSL3LhSeZ8dcnMwAmA",
-                type:"video",
-                q:this.state.text,
-                maxResults:maxresult,
-                controls:1
-            }}).then((res) => {
-
-        
-            this.setState({video:res.data.items[0],things:""});
-                this.setState({title:res.data.items[0].snippet.title})
-                this.setState({videoList:[]});
-            res.data.items.map(ele=>{
-                
-                this.setState({videoList:[...this.state.videoList,ele]});
-            return null;
-            });
-            
-
-           
-            return (res.data);
-        }).catch(err => console.log(err));
-        }
-    
-    
-
-
-
-
-
-
-componentDidMount()
-{
-
-
-this.youtube();
-
-
-}
-show = () => {
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      things: "Loading...",
+      videoList: [],
+      toggle: "Show",
+      video: {},
+      description: ``,
+      text: "covid 19 news",
+      title: "",
    
+    };
+    this.goup =createRef();
+  }
+  youtube = async () => {
+    await axios
+      .get(`https://www.googleapis.com/youtube/v3/search`, {
+        params: {
+          part: "snippet",
+          key: "AIzaSyCranKkgzC8Bui0dlSL3LhSeZ8dcnMwAmA",
+          type: "video",
+          q: this.state.text,
+          maxResults: maxresult,
+          controls: 1,
+        },
+      })
+      .then((res) => {
+        this.setState({ video: res.data.items[0], things: "" });
+        this.setState({ title: res.data.items[0].snippet.title });
+        this.setState({ videoList: [] });
+        res.data.items.map((ele) => {
+          this.setState({ videoList: [...this.state.videoList, ele] });
+          return null;
+        });
 
+        return res.data;
+      })
+      .catch((err) => console.log(err));
+  };
+
+  componentDidMount() {
+    this.youtube();
     
-    if (this.state.things==="Loading...")
-    {
-        return <div>Loading...</div>
-    } 
-        
-    else {
-        return (
-
-            <div>
-
-                <iframe title={this.state.video.id.videoId} width="50%" height="400" src={`https://www.youtube.com/embed/${this.state.video.id.videoId}`} frameBorder="0" allowFullScreen >
-                </iframe>
-                <h1>
-
-                </h1>
-            </div>
-
-
-
-        )
-
-    }
-
-
-
-}
-showit=()=>
-               {
-            
-                if(this.state.toggle==="Show")
-                {
-                    
-                
-                        this.setState({toggle:"Hide"});
-                this.setState({description:this.state.video.snippet.description})
-                }
-                else{
-                    
-                    this.setState({toggle:"Show"});
-                    this.setState({description:``});
-                    
-                }
-        
-               
-
-                
-            }        
-butclickhandel=()=>
-{
-
-    
-    this.showit();
-
-}
-searchhanddler=(tfs)=>
-{
-    
-   
-    if(tfs)
-        {
-           
-  this.setState({text:tfs},()=>
-  {
-    
-        this.youtube();
-
-    
-    
-  });
-  console.log(this.state.videoList);
   
-
-
-
-
-
-
-
-    }
-    else if(tfs==="")
-    {
-        console.log("please write something");
-    }
-
-
-}
-setselectedvideo=(singlevideo)=>
-{console.log(singlevideo);
-    this.setState({video:singlevideo,title:singlevideo.snippet.title});
-
-
-}
-
-render()
-{
-    
-        return (
-        
-        
+          
+  }
+  show = () => {
+    if (this.state.things === "Loading...") {
+      return <div>Loading...</div>;
+    } else {
+      return (
         <div>
-<Search searchhanddler={(tfs)=>this.searchhanddler(tfs)}/>
+          <iframe
+            title={this.state.video.id.videoId}
+            width="50%"
+            height="400"
+            src={`https://www.youtube.com/embed/${this.state.video.id.videoId}`}
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+         
+        </div>
+      );
+    }
+  };
+  showit = () => {
+    if (this.state.toggle === "Show") {
+      this.setState({ toggle: "Hide" });
+      this.setState({ description: this.state.video.snippet.description });
+    } else {
+      this.setState({ toggle: "Show" });
+      this.setState({ description: `` });
+    }
+  };
+  butclickhandel = () => {
+    this.showit();
+  };
+  searchhanddler = (tfs) => {
+    if (tfs) {
+      this.setState({ text: tfs }, () => {
+        this.youtube();
+      });
+      console.log(this.state.videoList);
+    } else if (tfs === "") {
+      console.log("please write something");
+    }
+  };
+  setselectedvideo = (singlevideo) => {
 
-        <div id={Styles.mainvideodiv}>
-            
-            <div id={Styles.mainvideo}> { this.show()} </div>
-           <h2 id={Styles.mainvideotitle}>{this.state.title} </h2>
-           <div id={Styles.maindescription}>
-    <button id={Styles.mainvideodescriptionbut} onClick={this.butclickhandel.bind(this)}>{`${this.state.toggle} description`}</button>
-        <p id={Styles.mainvideodescription}>{this.state.description }<br/></p>
-        
-               
-           </div>
+    this.setState({ video: singlevideo, title: singlevideo.snippet.title });
+    this.goup.scrollIntoView({behavior:"smooth"})
+  };
 
+  render() {
+    return (
+      <div>
+        <Search  searchhanddler={(tfs) => this.searchhanddler(tfs)} />
+
+        <div ref={ref=>this.goup=ref} id={Styles.mainvideodiv}>
+          <div id={Styles.mainvideo}> {this.show()} </div>
+          <h2 id={Styles.mainvideotitle}>{this.state.title} </h2>
+          <div id={Styles.maindescription}>
+            <button
+              id={Styles.mainvideodescriptionbut}
+              onClick={this.butclickhandel.bind(this)}
+            >{`${this.state.toggle} description`}</button>
+            <p id={Styles.mainvideodescription}>
+              {this.state.description}
+              <br />
+            </p>
+          </div>
         </div>
         <div>
-       
-    
-        {this.state.videoList[maxresult-1]?<List getthevideo={this.setselectedvideo} videolist={this.state.videoList}/>:null}
-        
-       
+          {this.state.videoList[maxresult - 1] ? (
+            <List
+              getthevideo={this.setselectedvideo}
+              videolist={this.state.videoList}
+            />
+          ) : null}
         </div>
-        </div>
-        
-    )
-}
-
+      </div>
+    );
+  }
 }
 export default Item;
--- src/components/List.js
+++ src/components/List.js
@@ -1,62 +1,42 @@
+import React from "react";
+import Styles from "./List.module.css";
 
-import React from 'react'
-import Styles from './List.module.css';
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videolist: props.videolist,
+    };
+  }
 
-class List extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state={
-            videolist:props.videolist
-            
-        }
-        
-    }
-
-goyaar=(ele)=>
-{
-
+  selectionhandle = (ele) => {
     this.props.getthevideo(ele);
-}
-render()
-{
-    
-        return(
-            <div id={Styles.mainlistdiv}>
-                {  
-                this.state.videolist.map((ele,id)=>{
-                    const videoId=ele.id.videoId;
-                    
-                    const videotitle=ele.snippet.title;
-                    return(
-                      
-          
- 
- <div key={id} onClick={this.goyaar.bind(this,ele)}  id={Styles.eachlistdiv}>
-                    <img id={Styles.eachlistimg}   src={`https://img.youtube.com/vi/${videoId}/mqdefault.jpg
-          `} alt="Thumbnail"></img>
-                    <h3 id={Styles.eachlisttitle}>{videotitle} </h3>
-               
-               
-                </div>
-        
-          
-           
-                    )
+  };
+  render() {
+    return (
+      <div id={Styles.mainlistdiv}>
+        {this.state.videolist.map((ele, id) => {
+          const videoId = ele.id.videoId;
 
-           
-                })}
-        
-        </div>
-        )
-    
-
-}
-    
-        
-    
-  
-    
+          const videotitle = ele.snippet.title;
+          return (
+            <div
+              key={id}
+              onClick={this.selectionhandle.bind(this, ele)}
+              id={Styles.eachlistdiv}
+            >
+              <img
+                id={Styles.eachlistimg}
+                src={`https://img.youtube.com/vi/${videoId}/mqdefault.jpg
+          `}
+                alt="Thumbnail"
+              ></img>
+              <h3 id={Styles.eachlisttitle}>{videotitle} </h3>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default List;
